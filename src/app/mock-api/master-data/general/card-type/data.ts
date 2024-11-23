14a9371ebdbb79dee7fb4428babe2608437d9f9e
nooele2 <noellehtoo@gmail.com>
2024-11-23 22:39:47 +0700

    card-type

--- src/app/mock-api/master-data/general/card-type/data.ts
+++ src/app/mock-api/master-data/general/card-type/data.ts
@@ -1,3 +1,8 @@
 export const cardTypes = [
-
+    {
+        id: 'b899ec30-b85a-40ab-bb1f-18a596d5c6de',
+        issuerID: '1',
+        name: 'Hebrew',
+        modifiedDate: 'April 24, 2018',
+    },
 ];
