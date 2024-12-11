ffed4fcc1b841e28aa14d05bff4e2261d92f8738
LuRyanXdi <101129742+LuRyanXdi@users.noreply.github.com>
2024-12-12 00:26:15 +0700

    dateErrorFixed

--- src/app/mock-api/master-data/general/currency/data.ts
+++ src/app/mock-api/master-data/general/currency/data.ts
@@ -4,21 +4,21 @@ export const currencies = [
         currencyCode: 'MMK',
         currencyName: 'Myanmar(MMK)',
         symbol: 'Ks',
-        lastEditedOn: 'April 24, 2018',
+        modifiedDate: 'April 24, 2018',
     },
     {
         id: '2',
         currencyCode: 'THB',
         currencyName: 'Thailand(THB)',
         symbol: 'B',
-        lastEditedOn: 'April 24, 2018',
+        modifiedDate: 'April 24, 2018',
     },
     {
         id: '3',
         currencyCode: 'USD',
         currencyName: 'American Dollar(USD)',
         symbol: '$',
-        lastEditedOn: 'April 24, 2018',
+        modifiedDate: 'April 24, 2018',
     },
 ];
 
