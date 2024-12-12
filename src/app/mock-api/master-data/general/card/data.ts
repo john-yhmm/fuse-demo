52f9b5629efb056d3e60998ba04c2e9c55284f2a
KhinKyawtShin <145042094+KhinKyawtShin@users.noreply.github.com>
2024-12-12 09:43:58 +0700

    added cardTypeID on list column

--- src/app/mock-api/master-data/general/card/data.ts
+++ src/app/mock-api/master-data/general/card/data.ts
@@ -2,25 +2,25 @@ export const cards = [
     {
         id:'1',
         cardNumber: '0000 0000',
-        cardTypeID: '1',
+        cardTypeID: '',
         modifiedDate: 'May 4, 2000',
     },
     {
         id:'2',
         cardNumber: '1111 1111',
-        cardTypeID: '1',
+        cardTypeID: '',
         modifiedDate: 'May 4, 2000',
     },
     {
         id:'3',
         cardNumber: '2222 2222',
-        cardTypeID: '1',
+        cardTypeID: '',
         modifiedDate: 'May 4, 2000',
     },
     {
         id:'4',
         cardNumber: '3333 3333',
-        cardTypeID: '1',
+        cardTypeID: '',
         modifiedDate: 'May 4, 2000',
     }
 ]
